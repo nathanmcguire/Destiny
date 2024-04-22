aea54a9cc259693e2504a1887e34df2c20f78d5b
Nathan C. McGuire <nathanmcguire@users.noreply.github.com>
2024-04-22 09:22:43 -0500

    Create Destiny.psm1

---
+++ Destiny/Destiny.psm1
@@ -0,0 +1,15 @@
+Using module ./Destiny/Destiny/Destiny.psm1
+
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+
+Foreach ($import in @($Public + $Private)) {
+    Try {
+        . $import.fullname
+    }
+    Catch {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+
+Export-ModuleMember -Function $Public.Basename
