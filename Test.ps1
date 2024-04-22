073fc89183f65d405b88f2a904c381caa946d696
Nathan C. McGuire <nathanmcguire@users.noreply.github.com>
2024-04-22 14:34:29 -0500

    Delete Test.ps1

--- Test.ps1
+++
@@ -1,6 +0,0 @@
-Clear
-Remove-Module Destiny
-Copy-Item .\Destiny\* ~\Documents\WindowsPowerShell\Modules\Destiny\1.0\ -Recurse -Force
-Import-Module Destiny
-. ./MyConfiguration.ps1
-New-DestinyAccessToken -Domain $Domain -Context $Context -ClientId $ClientId -ClientSecret $ClientSecret
