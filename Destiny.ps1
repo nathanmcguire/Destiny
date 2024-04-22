6e5a007a97b253c039409b98115816ee0cb0eb36
Nathan C. McGuire <nathanmcguire@users.noreply.github.com>
2024-04-22 15:01:31 -0500

    Update Destiny.ps1

--- Destiny.ps1
+++ Destiny.ps1
@@ -1,73 +1,86 @@
 . ./MyConfiguration.ps1
-
 $baseURL = "https://$Domain/api/v1/rest/context/$Context"
-
-# Function to get grants
-function Get-DestinyGrants {
-    param(
-        [string]$appId,
-        [string[]]$appFamilyIds,
-        [string[]]$principalIds,
-        [string]$subcontextId
-    )
-
-    # Construct the URL
-    $url = "$baseURL/auth/grants"
-
-    # Construct query parameters
-    $queryParams = @{
-        appId = $appId
-        appFamilyIds = $appFamilyIds -join ','
-        principalIds = $principalIds -join ','
-        subcontextId = $subcontextId
-    }
-
-    # Make the API request
-    $response = Invoke-RestMethod -Uri $url -Method Get -Query $queryParams
-
-    # Return the response
-    return $response
-}
-
-# Function to get access token
 function Get-DestinyAccessToken {
     param(
         [string]$clientId,
         [string]$clientSecret
     )
-
-    # Construct the URL
     $url = "$baseURL/auth/accessToken"
-
-    # Construct the request body
     $body = @{
         grant_type = 'client_credentials'
         client_id = $clientId
         client_secret = $clientSecret
     }
-
-    # Make the API request
     $response = Invoke-RestMethod -Uri $url -Method Post -Body $body
-
-    # Return the response
     return $response
 }
-
-# Function to get public key
-function Get-DestinyPublicKey {
-    param(
-        [string]$name
+function Get-Patron {
+    param (
+        [string]$accessToken,
+        [string]$id,
+        [string]$siteId = ""
     )
-
-    # Construct the URL
-    $url = "$baseURL/auth/publickey/$name"
-
-    # Make the API request
-    $response = Invoke-RestMethod -Uri $url -Method Get
-
-    # Return the response
+    if ($siteId -eq "") {
+        $url = "$baseURL/patrons/$id"
+    } else {
+        $url = "$baseURL/sites/$siteId/patrons/$id"
+    }
+    Invoke-RestMethod -Uri $url -Method Get -Headers @{ "Authorization" = "Bearer $accessToken" }
+}
+function Get-Sites {
+    param (
+        [string]$accessToken,
+        [string]$id,
+        [string[]]$productTypes
+    )
+    # If $id is provided, retrieve a specific site
+    if ($id) {
+        $url = "$baseUrl/sites/$id"
+    }
+    # If $productTypes is provided, retrieve sites based on product types
+    elseif ($productTypes) {
+        $url = "$baseUrl/sites?productTypes=$($productTypes -join '&productTypes=')"
+    }
+    # If neither $id nor $productTypes is provided, retrieve all sites
+    else {
+        $url = "$baseUrl/sites"
+    }
+    $response = Invoke-RestMethod -Uri $url -Headers @{Authorization = "Bearer $accessToken"} -Method Get
     return $response
 }
-
-# Export functions
-Export-ModuleMember -Function Get-DestinyGrants, Get-DestinyAccessToken, Get-DestinyPublicKey
+function Get-Fines {
+    param (
+        [string]$accessToken,
+        [string]$districtId,
+        [string]$patronBarcode,
+        [string]$siteId,
+        [string]$collectionType
+    )
+    $url = "$baseURL/fines"
+    $queryParams = @{}
+    if ($districtId) { $queryParams['districtId'] = $districtId }
+    if ($patronBarcode) { $queryParams['patronBarcode'] = $patronBarcode }
+    if ($siteId) { $queryParams['siteId'] = $siteId }
+    if ($collectionType) { $queryParams['collectionType'] = $collectionType }
+    $headers = @{
+        "Authorization" = "Bearer $accessToken"
+    }
+    $response = Invoke-RestMethod -Uri $url -Method Get -Headers $headers -Query $queryParams
+    return $response
+}
+function Pay-Fines {
+    param (
+        [string]$accessToken,
+        [array]$finePayments
+    )
+    $url = "$baseURL/fines/payments"
+    $headers = @{
+        "Authorization" = "Bearer $accessToken"
+    }
+    $response = Invoke-RestMethod -Uri $url -Method Post -Headers $headers -Body ($finePayments | ConvertTo-Json)
+    return $response
+}
+# Example usage:
+# $accessToken = "your_access_token_here"
+# Get-Fines -accessToken $accessToken -districtId "2020-789"
+# Pay-Fines -accessToken $accessToken -finePayments @(@{fineId=224; externalPaymentId='001-23438F'; payment=2.37})
