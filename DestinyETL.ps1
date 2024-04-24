e2ed540d909ec9f79e60855d2cd2d13b591e2867
Nathan C. McGuire <nathanmcguire@users.noreply.github.com>
2024-04-24 14:19:39 -0500

    Update DestinyETL.ps1

--- DestinyETL.ps1
+++ DestinyETL.ps1
@@ -1,10 +1,10 @@
 . ./MyConfiguration.ps1
 . ./Destiny.ps1
 Clear
-
 $DestinyAccessToken = Get-DestinyAccessToken -ClientId $ClientId -ClientSecret $ClientSecret -Domain $Domain -Context $Context
+
+
 $DestinyFine = Get-DestinyFine -AccessToken $DestinyAccessToken.access_token -Domain $Domain -Context $Context
-# Iterate through each record in $DestinyFine
 $DestinyFine | Foreach {
     $FineId = $_.fineId
     $SiteId = $_.siteId
@@ -25,7 +25,6 @@ $DestinyFine | Foreach {
     $PaymentReversed = $_.paymentReversed
     $WaiverReversed = $_.waiverReversed
 
-    # Check if the record already exists in the database
     $query = "SELECT fineId FROM Fine WHERE fineId = $FineId"
     $existingRecordsCount = (Invoke-SqlCmd -ServerInstance $SQLServer -Database $SQLDatabase -Query $query).Count
     if ($existingRecordsCount -eq 0) {
@@ -34,7 +33,7 @@ $DestinyFine | Foreach {
             SET IDENTITY_INSERT Fine ON;
             INSERT INTO Fine (
                 [fineId],[siteId],[patronId],[collectionType],[type],
-                [destinyDateCreated],[description],[bibId],[copyId],[amountDue],
+                [dateCreated],[description],[bibId],[copyId],[amountDue],
                 [amount],[paid],[waived],[refundAmount],[refundPaid],
                 [currencyUnit],[paymentReversed],[waiverReversed])
             VALUES (
@@ -54,7 +53,7 @@ $DestinyFine | Foreach {
             patronId = '$PatronId',
             collectionType = '$CollectionType',
             type = '$Type',
-            DestinyDateCreated = '$DateCreated',
+            dateCreated = '$DateCreated',
             description = '$Description',
             bibId = '$BibId',
             copyId = '$CopyId',
@@ -70,5 +69,79 @@ $DestinyFine | Foreach {
             WHERE fineId = $FineId
         "
         Invoke-SqlCmd -ServerInstance $SQLServer -Database $SQLDatabase -Query $query -Verbose
+    }
+}
+
+$DestinyPatron = Get-DestinyPatron -AccessToken $DestinyAccessToken.access_token -Domain $Domain -Context $Context -PatronId 4576
+$DestinyPatron | foreach {
+    $PatronId = $_.internalId
+    $DistrictId = $_.districtId
+    $LastName = $_.lastName
+    $FirstName = $_.firstName
+    $Nickname = $_.nickname
+    $GradeLevel = $_.gradeLevel
+    $SiteAssociation = $_.siteAssociations
+    $SiteAssociation
+    $query = "SELECT patronId FROM Patron WHERE patronId = $PatronId"
+    $existingRecordsCount = (Invoke-SqlCmd -ServerInstance $SQLServer -Database $SQLDatabase -Query $query).Count
+    if ($existingRecordsCount -eq 0) {
+        $query = "
+            SET IDENTITY_INSERT Patron ON;
+            INSERT INTO Patron (
+                [patronId],[districtid],[lastName],[firstName],[middleName],
+                [nickName],[gradeLevel])
+            VALUES (
+                '$PatronId','$DistrictId','$LastName','$FirstName','$MiddleName',
+                '$Nickname','$gradeLevel');
+            SET IDENTITY_INSERT Patron OFF;
+        "
+        Invoke-SqlCmd -ServerInstance $SQLServer -Database $SQLDatabase -Query $query -Verbose
+    } else {
+        $query = "
+            UPDATE Patron
+            SET 
+            districtId = '$DistrictId',
+            lastName = '$LastName',
+            firstName = '$FirstName',
+            middleName = '$MiddleName',
+            nickName = '$NickName',
+            gradeLevel = '$GradeLevel'
+            WHERE patronId = $PatronId
+        "
+        Invoke-SqlCmd -ServerInstance $SQLServer -Database $SQLDatabase -Query $query -Verbose
+    }
+    $SiteAssociation | foreach {
+        $SiteId = $_.siteId
+        $Barcode = $_.barcode
+        $PrimarySite = $_.primarySite
+        $Type = $_.type
+        $Teacher = $_.teacher
+        $Status = $_.status
+        $query = "SELECT siteAssociationId  FROM SiteAssociation WHERE patronId = $PatronId AND siteId = $SiteId"
+        $existingRecordsCount = (Invoke-SqlCmd -ServerInstance $SQLServer -Database $SQLDatabase -Query $query).Count
+        if ($existingRecordsCount -eq 0) {
+            $query = "
+                INSERT INTO SiteAssociation (
+                    [patronId],[siteId],[barcode],[primarySite],[type],
+                    [teacher],[status])
+                VALUES (
+                    '$PatronId','$SiteId','$Barcode','$PrimarySite','$Type',
+                    '$Teacher','$Status');
+            "
+            Invoke-SqlCmd -ServerInstance $SQLServer -Database $SQLDatabase -Query $query -Verbose
+        } else {
+            $query = "
+                UPDATE SiteAssociation
+                SET 
+                barcode = '$Barcode',
+                primarySite = '$PrimarySite',
+                type = '$Type',
+                teacher = '$Teacher',
+                Status = '$Status'
+                WHERE patronId = $PatronId AND siteId = $SiteId
+            "
+            Invoke-SqlCmd -ServerInstance $SQLServer -Database $SQLDatabase -Query $query -Verbose
+        }
+        
     }
 }
